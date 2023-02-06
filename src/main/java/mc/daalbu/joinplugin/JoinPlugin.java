6e9666e6af68a6dd8a7c8bda812c987aa4da4f18
DaalbuCZ <buresd@gytool.cz>
2023-02-06 21:06:44 +0100

    Initial commit Add main class Add plugin.yml

---
+++ src/main/java/mc/daalbu/joinplugin/JoinPlugin.java
@@ -0,0 +1,17 @@
+package mc.daalbu.joinplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JoinPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
